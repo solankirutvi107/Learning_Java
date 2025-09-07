a4541f8de1f2a896d4dc0d3c4f8684986f9ebb5e
Rutvi <solankirutvi107@gmail.com>
2025-09-07 11:23:36 +0530

    Basic java hello world

--- src/Lab_001.java
+++
@@ -1,8 +0,0 @@
-public class Lab_001 {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-    }
-}
---
+++ src/ex01_javabasic/Lab_001.java
@@ -0,0 +1,7 @@
+package ex01_javabasic;
+
+public class Lab_001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
