7c01e8b7e62a793cc450049d072fc6c7feb63358
Rutvi <solankirutvi107@gmail.com>
2025-09-13 10:00:41 +0530

    Basic java comment and math

---
+++ src/ex02_Java_basic/Lab_002_Comment.java
@@ -0,0 +1,11 @@
+package ex02_Java_basic;
+
+public class Lab_002_Comment {
+    public static void main(String[] args) {
+        //test single  line comment
+        /**
+         test
+         **/
+        System.out.println("Single and multiple  comment");
+    }
+}
---
+++ src/ex02_Java_basic/Lab_002_math.java
@@ -0,0 +1,8 @@
+package ex02_Java_basic;
+
+public class Lab_002_math {
+
+    public static void main(String[] args) {
+        System.out.println(10+3/2*2-1);
+    }
+}
